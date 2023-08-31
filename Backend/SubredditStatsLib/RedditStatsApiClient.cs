b4163af3e524dc1249653319f9a9b426dfe170bc
Nathan Miller <nmiller217@gmail.com>
2023-08-31 14:17:08 -0700

    add enum

--- Backend/SubredditStatsLib/RedditStatsApiClient.cs
+++ Backend/SubredditStatsLib/RedditStatsApiClient.cs
@@ -14,37 +14,69 @@ namespace SubredditStats.Backend.Lib
 {
     public class RedditStatsApiClient : ISubredditStatsSource
     {
+        private const string RedditApiUri = "https://oauth.reddit.com";
+        private const string SubredditPostListingUriFormat = RedditApiUri + "/r/{0}/{1}";
+
         private RedditApiToken? _currentAccessToken;
+
+        public enum PostListingSortType
+        {
+            top,
+            @new
+        }
 
         public RedditStatsApiClient()
         {
             _currentAccessToken = null;
         }  
 
-        public async Task<RedditPostListing?> GetMostUpvotedPosts(string subreddit)
+        public async Task<RedditPostListing?> FetchSubredditPostListing(string subreddit, PostListingSortType sort)
+        {
+            using var httpClient = await CreateHttpClientAsync();
+            if (httpClient is not null)
+            {
+                var uri = string.Format(SubredditPostListingUriFormat, subreddit, sort.ToString());
+                var response = await httpClient.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    var s = await response.Content.ReadAsStringAsync();
+                    var redditPostListing = JsonSerializer.Deserialize<RedditPostListing>(s);
+                    return redditPostListing;
+
+                }
+            }
+            return null;
+        }     
+
+        public async Task<RedditPostListing?> GetSubredditTopPosts(string subreddit)
+        {
+            return await FetchSubredditPostListing(subreddit, PostListingSortType.top);
+        }
+
+        public async Task<RedditPostListing?> GetSubredditNewPosts(string subreddit)
+        {
+            return await FetchSubredditPostListing(subreddit, PostListingSortType.@new);
+        }
+
+        // returning HTTP 403 Forbidden (probably requires mod persmission)
+        public async Task<string> GetAboutContributors(string subreddit)
         {
             using var httpClient = await CreateHttpClientAsync();
             if (httpClient is not null)
             {
                 //var redditPostListing = await httpClient.GetFromJsonAsync<RedditPostListing>($"https://oauth.reddit.com/r/{subreddit}/top");
                 //return redditPostListing;
-                var response = await httpClient.GetAsync($"https://oauth.reddit.com/r/{subreddit}/top");
+                var response = await httpClient.GetAsync($"https://oauth.reddit.com/r/{subreddit}/about/contributors");
                 if (response.IsSuccessStatusCode)
                 {
                     var s = await response.Content.ReadAsStringAsync();
-                    var redditPostListing = JsonSerializer.Deserialize<RedditPostListing>(s, GlobalJsonSerializerOptions.Options);
-                    return redditPostListing;
+                    //var redditPostListing = JsonSerializer.Deserialize<RedditPostListing>(s, GlobalJsonSerializerOptions.Options);
+                    return s;
 
                 }
             }
             return null;
-        }
-
-        //public async Task<List<string>> GetMostPostingUsers(string subreddit)
-        //{
-        //    using var httpClient = await CreateHttpClientAsync();
-
-        //}
+        }        
 
         private async Task<HttpClient?> CreateHttpClientAsync()
         {
