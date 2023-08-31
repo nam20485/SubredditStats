6d3da668095c625adf988d71d6e6fc298c7fa110
Nathan Miller <nmiller217@gmail.com>
2023-08-31 11:23:19 -0700

    add endpoint for fecthing most upvoted posts for a specified subreddit

---
+++ Backend/WebApi/Controllers/SubredditStatsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SubredditStats.Backend.Lib;
+using SubredditStats.Shared.Model;
+
+namespace SubredditStats.Backend.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SubredditStatsController : ControllerBase
+    {
+        private readonly RedditStatsApiClient _redditApiClient;
+
+        public SubredditStatsController()
+        {
+            _redditApiClient = new RedditStatsApiClient();
+        }
+
+        [HttpGet("mostupvotedposts/{subreddit}")]
+        public async Task<ActionResult<RedditPostListing?>> GetMostUpvotedPostsAsync([FromRoute] string subreddit)
+        {
+            return await _redditApiClient.GetMostUpvotedPosts(subreddit);
+        }
+    }
+}
