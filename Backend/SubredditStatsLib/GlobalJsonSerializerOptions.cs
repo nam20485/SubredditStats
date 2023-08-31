8293052f4e02c979ee865a5e583619dee139f70d
Nathan Miller <nmiller217@gmail.com>
2023-08-31 11:21:55 -0700

    use Web defaults for JsonSerializerOptions

---
+++ Backend/SubredditStatsLib/GlobalJsonSerializerOptions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SubredditStats.Backend.Lib
+{
+    public static class GlobalJsonSerializerOptions
+    {
+        public static JsonSerializerOptions Options { get; } = new JsonSerializerOptions(JsonSerializerDefaults.Web)
+        {
+            // nothing
+        };
+    }
+}
