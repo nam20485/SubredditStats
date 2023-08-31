bd69626ab14261f0af59e3a15666a39878c5be53
Nathan Miller <nmiller217@gmail.com>
2023-08-31 11:22:46 -0700

    encapsulate access token fetching in auth class

---
+++ Backend/SubredditStatsLib/RedditApiAuth.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SubredditStats.Backend.Lib
+{
+    public class RedditApiAuth
+    {
+        public static string? ClientId => Environment.GetEnvironmentVariable("REDDIT_API_CLIENT_ID");
+        public static string? ClientSecret => Environment.GetEnvironmentVariable("REDDIT_API_CLIENT_SECRET");
+        public static string AppName => "subrpoststats";
+
+        private const string TokenUrl = "https://www.reddit.com/api/v1/access_token";
+
+        public const string UserAgent = "subrpoststats";
+
+        public static async Task<RedditApiToken?> FetchAccessToken()
+        {
+            using var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, TokenUrl);
+            var authHeader = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{ClientId}:{ClientSecret}"));
+            request.Headers.Authorization = new AuthenticationHeaderValue("Basic", authHeader);
+            request.Headers.UserAgent.Add(new ProductInfoHeaderValue(UserAgent, "1.0"));
+            request.Content = new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                {"grant_type", "client_credentials"}
+            });
+            var response = await client.SendAsync(request);
+            var token = await response.Content.ReadFromJsonAsync<RedditApiToken>();
+            return token;
+        }
+    }
+}
---
+++ Backend/SubredditStatsLib/RedditApiToken.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SubredditStats.Backend.Lib
+{
+    public class RedditApiToken
+    {
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonPropertyName("token_type")]
+        public string TokenType { get; set; }
+
+        [JsonPropertyName("expires_in")]
+        public int ExpiresInS { get; set; }
+
+        [JsonPropertyName("scope")]
+        public string Scope { get; set; }
+
+        [JsonIgnore]
+        public DateTime RetrievedAt { get; set; } = DateTime.UtcNow;
+
+        [JsonIgnore]
+        public DateTime ExpiresAt => RetrievedAt.AddSeconds(ExpiresInS);
+
+        [JsonIgnore]
+        public bool IsExpired => ExpiresAt <= DateTime.UtcNow;
+    }
+}
