344b1e87ad9ac43d0dd0d19362ad71cb394a05fc
Nathan Miller <nmiller217@gmail.com>
2023-09-01 11:28:44 -0700

    add unimplemented empty example store type class

---
+++ Backend/SubredditStatsLib/Store/JsonFileStore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SubredditStats.Shared.Model;
+
+namespace SubredditStats.Backend.Lib.Store
+{
+    public class JsonFileStore : ISubredditPostsStatsStore
+    {
+        // e.g. save data and load data from JSON file
+
+        public MostPosterInfo[] MostPosters => throw new NotImplementedException();
+
+        public TopPostInfo[] TopPosts => throw new NotImplementedException();
+
+        public void AddMostPosters(IEnumerable<MostPosterInfo> mostPosters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddTopPosts(IEnumerable<TopPostInfo> topPosts)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
