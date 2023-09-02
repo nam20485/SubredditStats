a9953eebc30557a3d6a254d79c2bba4f9865383e
Nathan Miller <nmiller217@gmail.com>
2023-09-02 14:35:48 -0700

    move RedditPostListing classes out of shared

--- Backend/SubredditStatsLib/RedditApi/RedditPostListing.cs
+++ Backend/SubredditStatsLib/RedditApi/RedditPostListing.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SubredditStats.Shared.Model
+namespace SubredditStats.Backend.Lib.RedditApi
 {  
     public class RedditPostListing
     {
