bc10daf93e1642130a30b0d95539402b82ff41b5
Nathan Miller <nmiller217@gmail.com>
2023-09-05 12:21:45 -0700

    add skeleton for some tests

---
+++ Backend/Tests/WebApplicationTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using FluentAssertions;
+using FluentAssertions.Web;
+using Microsoft.AspNetCore.Mvc.Testing;
+using SubredditStats.Backend.WebApi;
+
+namespace Tests
+{
+    public class WebApplicationTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _webApplicationFactory;
+
+        public WebApplicationTests(WebApplicationFactory<Program> webApplicationFactory)
+        {
+            _webApplicationFactory = webApplicationFactory;
+        }
+     
+        [Theory]
+        [InlineData("https://localhost:5001/api/subredditstats/top_posts/10")]
+        public void Test_Endpoints(string uri)
+        {
+
+        }
+    }
+}
