ca27cc82ab0937eb4707a2882d47aa07f8c4b5a8
Nathan Miller <nmiller217@gmail.com>
2023-09-05 17:53:38 -0700

    add MemoryStore derived class for testing

---
+++ Backend/Tests/TestMemoryStore.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SubredditStats.Backend.Lib.Store;
+using SubredditStats.Shared.Model;
+
+namespace Tests
+{
+    internal class TestMemoryStore : MemoryStore
+    {
+        internal TestMemoryStore()
+        {
+            AddPostInfos(PostInfo.List.CreateRandom(100));
+            SetTopPosters(PostInfo.List.CreateRandom(100));
+            SetMostPosters(MostPosterInfo.List.CreateRandom(100));
+        }
+    }
+}
