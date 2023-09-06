7900d6fadfb1db4f09987d82b8f41fc150728c01
Nathan Miller <nmiller217@gmail.com>
2023-09-05 17:55:18 -0700

    add commented out test class for api client

---
+++ Backend/Tests/SubRedditPostStatsClientTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FluentAssertions;
+
+using Microsoft.AspNetCore.Mvc.Testing;
+
+using SubredditStats.Backend.WebApi;
+using SubredditStats.Shared.Client;
+using SubredditStats.Shared.Model;
+
+using Xunit;
+
+namespace Tests
+{
+    public class SubRedditPostStatsClientTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private const int ResponseObjectCount = 100;
+
+        private readonly WebApplicationFactory<Program> _webApplicationFactory;
+
+        public SubRedditPostStatsClientTests(WebApplicationFactory<Program> webApplicationFactory)
+        {
+            _webApplicationFactory = webApplicationFactory;
+        }
+
+        //[Fact]
+        //public void ApiClient_AllPostInfos_ShouldResponseSuccessfully()
+        //{
+        //    var subredditStatsClient = TestUtils.CreateSubRedditPostStatsApiClient(_webApplicationFactory, ResponseObjectCount);
+        //    var allPosts = subredditStatsClient.AllPostInfos;
+        //    allPosts.Should().NotBeNull();
+        //    allPosts.Should().NotBeEmpty();
+        //    allPosts.Should().HaveCount(ResponseObjectCount);
+        //    allPosts.Should().BeOfType(typeof(PostInfo[]));
+        //}         
+    }
+}
